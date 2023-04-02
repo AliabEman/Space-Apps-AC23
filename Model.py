a9f45fbf4e5e261cc812278ba4a40bdc20c43cfd
GregRowat <gregrowat@gmail.com>
2023-04-02 11:59:48 -0400

    fixed missing refernce to efficiency, added devtest set used in tets plan, fixed the ordering of the code Dylan added as it was orphaned during the merge.

--- Model.py
+++ Model.py
@@ -9,13 +9,13 @@ class Model:
         # instantiate planet objects to add to the Model list via the CSV data dictionary on initialization
         for data in planet_data:
             if data['name'] != 'name':
-                 planet = Planet(name=data['name'], mass=data['mass'], distance=data['distance'])
-                 self.planets.append(planet)
-
+                planet = Planet(name=data['name'], mass=data['mass'], distance=data['distance'])
+                self.planets.append(planet)
 
         # create a duplicate list so the original data can be filtered or retrieved without harm
         self.filteredPlanets = []
         self.filteredPlanets = self.planets
 
         # will probably need some fields to save model data in order to pass it to the algorithm?
-        self.selected_planet = None 
+        self.selected_planet = None
+        self.efficiency_index = 1
