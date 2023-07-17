cd51df32583eca66c699daac43e2a7eefe705bd4
Dylan Carroll <carr0676@algonquinlive.com>
2023-07-17 16:17:49 -0400

    Added in graph from live data from calculation.

--- Model.py
+++ Model.py
@@ -1,6 +1,8 @@
 from Planet import Planet
 import time
-
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
 
 class Model:
 
@@ -46,7 +48,10 @@ class Model:
         start_time = time.time()                  # Start the timer
         data = []                                 # This is a list to store algorithm results for visualization
 
-       
+        #variables for graphing
+        counter = 0
+        interval_distances_km = []
+        interval_velocities_km_s = [] 
         #  loop that increments of distance equal to step, calculates the time that was taken to reach the next increment
         #  Then recalculate the new velocity and repeat. The speed will increase with distance.
         while distanceKM < max_distance:
@@ -64,7 +69,18 @@ class Model:
                 'distance': distance,
                 'velocity': velocity,
             })
+            #Appending values for the graph, using each 1000th value
+            if counter % 1000 == 0:
+              interval_distances_km.append(distanceKM) # stores distances
+              interval_velocities_km_s.append(velocity) # stores velocity
+            #  The data is being stored in a list for later visualization.
+            #  This will also be passed to the view
+            data.append({
+                'distance': distance,
+                'velocity': velocity,
+            })
             numCalc +=1
+            counter +=1
         end_time = time.time()  # Stop the timer
         t = '{:.5g}'.format(t) # format scientific notation of time to be limited to 5 digits
         t = str(t) + " years"
@@ -73,6 +89,46 @@ class Model:
 
         step = '{:.5g}'.format(step) # format scientific notation representing step size to be limited to 5 digits
         step = str(step) 
-
+        
         #  Send formatted value to view
+        
+        # size graph
+        plt.figure(figsize=(15, 10))
+        # this will take all of the distances and velocities and plot them
+        plt.plot(interval_distances_km, interval_velocities_km_s, label='Velocity', color='blue', linestyle='-')
+        plt.xlabel('Distance (km)') # label x axis
+        plt.ylabel('Velocity (km/s)') # label y axis
+        plt.title(str(selected_planet) + ': Velocity vs Distance')
+        plt.grid(True) # set to grid 
+
+        plt.xscale('log') # logarithmic scale to spread out data evenly(many points very close together)
+        #why log scale? to spread out points and make the pattern more clear
+        #this aids in visualization with large data sets
+
+        #add information about the velocity and effeciency index to the graph
+        plt.plot([], [], label=f'Starting Velocity: {starting_velocity:.2e} km/s', color='black', linestyle='-')
+        plt.plot([],[],   label=f'Effeciency Index:{self.efficiency_index}',color='black', linestyle='-')
+        #plot the legend
+        plt.legend()
+
+
+        #the function of these next two code blocks are to improve the readability
+        #of the graph, forcing even spacing on the axis and helping spread data points
+        num_x_ticks = 5  # Number of ticks on the x-axis
+        x_ticks = np.logspace(np.log10(interval_distances_km[0]), np.log10(max_distance), num=num_x_ticks)
+        plt.xticks(x_ticks, ['{:.1e}'.format(x) for x in x_ticks])#set x-axis position
+
+        num_y_ticks = 5  # Number of ticks on the y-axis
+        y_ticks = np.linspace(0, max(interval_velocities_km_s), num=num_y_ticks)
+        plt.yticks(y_ticks, ['{:.1e}'.format(y) for y in y_ticks])#set y axis position
+
+
+
+        # Force the limits for each axis. Set from 0 to max distance / velocity
+        plt.xlim(0, max_distance)
+        plt.ylim(0,max(interval_velocities_km_s))
+
+       
+        # Show the plot
+        plt.show()
         controller_reference.view.create_visualization_screen(t, calc, starting_velocity, numCalc, step)
