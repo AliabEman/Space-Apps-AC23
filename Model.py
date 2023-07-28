abb0e7a5e15c2028676781e51616b6de3f703b1c
greg rowat <gregrowat@gmail.com>
2023-07-28 12:54:32 -0400

    finished testing of API, required change of api connection and query to access correct dataset and eliminate invalid data. The API no longer crashes and returns expected dataset.

--- Model.py
+++ Model.py
@@ -3,19 +3,17 @@ import time
 import requests
 import json
 
+
 class Model:
 
     def __init__(self, planet_data):
         self.planets = []
 
-        ##
         # URL for the TAP API endpoint
-        # url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync"
-        url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+*+from+pscomppars&format=json"
+        url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync"
 
         # Construct the query to select all planets that
-        query = "SELECT pl_name, pl_msinie, sy_dist FROM ps WHERE sy_dist IS NOT NULL ORDER BY pl_name"
-
+        query = "SELECT pl_name, pl_bmasse, sy_dist FROM pscomppars WHERE sy_dist IS NOT NULL AND pl_bmasse IS NOT NULL ORDER BY pl_name"
 
         # Parameters for the API call
         params = {
@@ -30,17 +28,10 @@ class Model:
 
             if response.status_code == 200:
                 data_list = json.loads(response.text)
-                #print(data_list)
-                
-                # Create a set to store planet names that have been added
-                added_planet_names = set()
 
                 for data in data_list:
-                    planet_name = data['pl_name']
-                    if planet_name not in added_planet_names:
-                        planet = Planet(name=data['pl_name'], mass=data['pl_msinie'], distance=data['sy_dist'])
-                        self.planets.append(planet)
-                        added_planet_names.add(planet_name)
+                    planet = Planet(name=data['pl_name'], mass=data['pl_bmasse'], distance=data['sy_dist'])
+                    self.planets.append(planet)
 
                 # Print the number of planets loaded
                 print("Planets Loaded from NASA Exoplanet Archive:", len(self.planets))
@@ -66,10 +57,8 @@ class Model:
             # Print the number of planets loaded
             print("Planets Loaded from NASA_PRODUCTION.csv:", len(self.planets))
 
-
-
-    ##
-      # create a duplicate list so the original data can be filtered or retrieved without harm
+        ##
+        # create a duplicate list so the original data can be filtered or retrieved without harm
         self.filteredPlanets = []
         self.filteredPlanets = self.planets
 
@@ -81,54 +70,53 @@ class Model:
     # calls create_visualization in the view with algorithm results to display
     def run_algorithm(self, controller_reference, selected_planet):
         hubble_constant = 69.8  # Current value for hubble constant
-        max_distance = 439999652819071048e+23 # Distance to Observable Universe Edge in KM 
+        max_distance = 439999652819071048e+23  # Distance to Observable Universe Edge in KM
         distance = selected_planet.distance  # Distance to selected planet in Parsecs
-        #Converted distances to planet being observed#
-        distanceKM = distance *(3.0857 * 10 ** 13) # Convert distance from PC to KM for loop
-        distanceMPC = distance / 1000000 # Convert distance from PC to MPC for velocity calculations
+        # Converted distances to planet being observed#
+        distanceKM = distance * (3.0857 * 10 ** 13)  # Convert distance from PC to KM for loop
+        distanceMPC = distance / 1000000  # Convert distance from PC to MPC for velocity calculations
 
-
-        numCalc = 0 # This is the number of calculations performed by the algorithm
+        numCalc = 0  # This is the number of calculations performed by the algorithm
 
         velocity = hubble_constant * distanceMPC  # Calculate the starting velocity
-        starting_velocity = velocity              #save initial velocity for display
+        starting_velocity = velocity  # save initial velocity for display
 
         max_distance = max_distance - distanceKM  # get just the distance from the planet to obervational universe edge, ignoring distance from earth to the planet
-      
-        #Step size in km for how long the planet will travel before each recalculation
-        step = (max_distance/2000000/(self.efficiency_index*0.1))              # Divides by index to lower km for recalculations the larger the index
 
-        t = 0                                     # this is to count years for the final result
-        start_time = time.time()                  # Start the timer
-        data = []                                 # This is a list to store algorithm results for visualization
+        # Step size in km for how long the planet will travel before each recalculation
+        step = (max_distance / 2000000 / (
+                self.efficiency_index * 0.1))  # Divides by index to lower km for recalculations the larger the index
 
-       
+        t = 0  # this is to count years for the final result
+        start_time = time.time()  # Start the timer
+        data = []  # This is a list to store algorithm results for visualization
+
         #  loop that increments of distance equal to step, calculates the time that was taken to reach the next increment
         #  Then recalculate the new velocity and repeat. The speed will increase with distance.
         while distanceKM < max_distance:
-            
             distanceKM += step  # Planet moves step KM away from earth
-            delta_t = 0 
+            delta_t = 0
             delta_t = step / velocity  # Time taken to reach the next increment
             # recalculate as the distance increases, thus its expansion rate must increase
-            velocity = hubble_constant * (distanceKM *3.2407792896664E-20)  #since distanceKM contains current distance, this is used and converted to mpc as appropriate
+            velocity = hubble_constant * (
+                    distanceKM * 3.2407792896664E-20)  # since distanceKM contains current distance, this is used and converted to mpc as appropriate
             t += delta_t  # Add the time taken for the current step to the total elapsed time
-        
+
             #  The data is being stored in a list for later visualization.
             #  This will also be passed to the view
             data.append({
                 'distance': distance,
                 'velocity': velocity,
             })
-            numCalc +=1
+            numCalc += 1
         end_time = time.time()  # Stop the timer
-        t = '{:.5g}'.format(t) # format scientific notation of time to be limited to 5 digits
+        t = '{:.5g}'.format(t)  # format scientific notation of time to be limited to 5 digits
         t = str(t) + " years"
         end_time = time.time()  # Stop the timer
         calc = end_time - start_time
 
-        step = '{:.5g}'.format(step) # format scientific notation representing step size to be limited to 5 digits
-        step = str(step) 
+        step = '{:.5g}'.format(step)  # format scientific notation representing step size to be limited to 5 digits
+        step = str(step)
 
         #  Send formatted value to view
         controller_reference.view.create_visualization_screen(t, calc, starting_velocity, numCalc, step)
